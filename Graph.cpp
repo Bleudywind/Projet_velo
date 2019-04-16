27b8aebd42e3453552af8a725ca9887a6c63a48b
Bleudywind <thom.ferrier@gmail.com>
2019-04-16 14:38:58 +0200

    test

--- Graph.cpp
+++ Graph.cpp
@@ -7,3 +7,20 @@
 //
 
 #include "Graph.hpp"
+
+graph::graph (std::vector <sommet> sommets, std::vector<arrete> arretes)
+: m_sommets(sommets), m_arretes(arretes)
+{}
+
+graph::graph ()
+{}
+
+std::vector<sommet> graph::Get_sommets()
+{
+    return m_sommets;
+}
+
+std::vector<arrete> graph::Get_arretes()
+{
+    return m_arretes;
+}
--- lecture_fichier.cpp
+++ lecture_fichier.cpp
@@ -7,3 +7,48 @@
 //
 
 #include <stdio.h>
+#include "Graph.hpp"
+#include <string>
+#include <fstream>
+
+graph creation_graph (std::string nom_graph)
+{
+    std::ifstream fichier("nom_graph.txt", std::ios::in);
+    
+    if(fichier)
+    {
+        std::vector<sommet> sommets;
+        sommet S1{0,0,0};
+        int nb;
+        int var;
+        
+        fichier >> nb;
+        
+        for (int i = 0; i < nb; ++i)
+        {
+            fichier >> var;
+            S1.changement_nb(var);
+            fichier >> var;
+            S1.changement_x(var);
+            fichier >> var;
+            S1.changement_y(var);
+        }
+        
+        fichier >> nb;
+        
+        for (int i = 0; i < nb; ++i)
+        {
+            <#statements#>
+        }
+        
+        fichier.close();
+    }
+    
+        
+    
+    
+    graph Graph;
+    
+    return Graph;
+    
+}
